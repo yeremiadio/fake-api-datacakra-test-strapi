6fa09c03df0b5d1245eaa5f28ee4715853c44681
yeremiadio <raikkonendio@gmail.com>
2024-12-10 16:44:37 +0700

    feat: add cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_KEY'),
+                api_secret: env('CLOUDINARY_SECRET'),
+            },
+            actionOptions: {
+                upload: {
+                    folder: env('CLOUDINARY_FOLDER'),
+                },
+                uploadStream: {},
+                delete: {},
+            },
+        },
+    },
+});
